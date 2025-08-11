84de726bec6207492a5919e6f901a77fae23169e
Marcos Castro <marcos.yk07@gmail.com>
2025-08-11 14:11:54 -0300

    feat: add QuestionAnswer

---
+++ src/components/QuestionAnswer/index.jsx
@@ -0,0 +1,11 @@
+import S from './styles.module.css'
+
+export function QuestionAnswer (props) {
+  return (
+    <button className={S.container}
+    onClick={(event) => props.handleAnswerQuestion(event, props.question, props.answer)}
+    >
+      {props.answer}
+    </button>
+  )
+}
